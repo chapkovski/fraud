0423a02940d514a12b8b23162809ce3eba439c29
chapkovski <chapkovski@gmail.com>
2020-11-27 15:47:27 +0300

    amost done with first ver

--- fraud/models.py
+++ fraud/models.py
@@ -21,21 +21,26 @@ Experiment on Electoral fraud
 class Constants(BaseConstants):
     name_in_url = 'fraud'
     players_per_group = None
-    num_rounds = 1
+    num_rounds = 10
     num_players = 7
     num_candidates = 2
     num_voters = num_players - num_candidates
     vote_cost = 1
-    fraud_cost = 1
+    fraud_cost = 15
     win_voter_payoff = 10
-    win_candidate_payoff = 10
+    win_candidate_payoff = 20
     parties = ['Red', 'Blue']
+    revealing_stubs = {False: 'No information available',
+                       True: 'Candidate from party {party} is planning to commit fraud'}
 
 
 class SingleMixin:
     @property
     def single(self):
         return self.session.config.get('single_player')
+
+    def single_role(self):
+        return self.session.config.get('role')
 
 
 class Subsession(SingleMixin, BaseSubsession):
@@ -51,7 +56,7 @@ class Subsession(SingleMixin, BaseSubsession):
 
     def group_by_arrival_time_method(self, waiting_players):
         if self.single:
-            return waiting_players[0]
+            return [waiting_players[0]]
 
 
 class Group(SingleMixin, BaseGroup):
@@ -63,6 +68,25 @@ class Group(SingleMixin, BaseGroup):
     # TODO: again this is for single player mode only. Maybe there is more smart way of dealing with this,
     # have no time to think about it at the moment
     other_votes = JsonField()
+    num_reds = models.IntegerField()
+    num_blues = models.IntegerField()
+    divider = models.IntegerField()
+    winner = models.StringField()
+
+    @property
+    def player(self):
+        if self.single:
+            return self.get_players()[0]
+        raise Exception('Cant find single player for multi-player setting')
+
+    def after_round_starts(self):
+
+        if self.single:
+            for p in self.get_players():
+                if p.role() == 'voter' or self.round_number == 1:
+                    p.party = random.choice(Constants.parties)
+                else:
+                    p.party = p.in_round(1).party
 
     def get_fraud(self, party):
         if self.single:
@@ -72,20 +96,87 @@ class Group(SingleMixin, BaseGroup):
         if self.single:
             return getattr(self, f'{party}_revealing')
 
+    def get_red_decision(self):
+        revealing = self.get_fraud_info('red')
+        return Constants.revealing_stubs.get(revealing).format(party='BLUE')
+
+    def get_blue_decision(self):
+        revealing = self.get_fraud_info('blue')
+        return Constants.revealing_stubs.get(revealing).format(party='RED')
+
     def after_fraud(self):
-        print('AFTER FRAUD')
+        if self.single:
+            if self.player.role() == 'voter':
+                self.red_fraud = random.choice([True, False])
+                self.blue_fraud = random.choice([True, False])
+            else:
+                fraud_decision = self.player.fraud
+                party = self.player.party
+                setattr(self, f'{party.lower()}_fraud', fraud_decision)
+                if party == 'Red':
+                    self.blue_fraud = random.choice([True, False])
+                else:
+                    self.red_fraud = random.choice([True, False])
 
     def after_fraud_revealing(self):
-        print('AFTER FRAUD REVEALING')
+        if self.single and self.single_role() == 'voter':
+            self.red_revealing = random.choice([True, False])
+            self.blue_revealing = random.choice([True, False])
 
     def after_voting(self):
-        self.other_votes = random.choices([False, True], k=Constants.num_voters - 1)
+        if self.single:
+            p = self.player
+            party = p.party
+            if p.role() == 'voter':
+                self.other_votes = random.choices([False, True], k=Constants.num_voters - 1)
+                reds = self.other_votes[:2]
+                blues = self.other_votes[2:]
+            else:
+                self.divider = random.randint(2, 3)
+                self.other_votes = random.choices([False, True], k=Constants.num_voters)
+                reds = self.other_votes[:self.divider]
+                blues = self.other_votes[self.divider:]
+            print('RED', reds, 'BLUE', blues)
+            if p.role() == 'voter':
+                vote = p.vote
+                if party == 'Red':
+                    reds.append(vote)
+                else:
+                    blues.append(vote)
+            self.num_reds = sum(reds)
+            self.num_blues = sum(blues)
+
+            if self.subsession.fraud:
+                if self.red_fraud:
+                    self.num_reds += 1
+                    self.num_blues = max([self.num_blues - 1, 0])
+                if self.blue_fraud:
+                    self.num_reds = max([self.num_reds - 1, 0])
+                    self.num_blues += 1
+            if self.num_blues > self.num_reds:
+                self.winner = 'Blue'
+            elif self.num_blues < self.num_reds:
+                self.winner = 'Red'
+            else:
+                self.winner = random.choice(Constants.parties)
+
+            if self.winner == p.party:
+                if p.role() == 'voter':
+                    p.payoff = Constants.win_voter_payoff - Constants.vote_cost * p.vote
+                else:
+                    p.payoff = Constants.win_candidate_payoff - Constants.fraud_cost * p.fraud
+            else:
+                if p.role() == 'voter':
+                    p.payoff = - Constants.vote_cost * p.vote
+                else:
+                    p.payoff = - Constants.fraud_cost * p.fraud
 
 
 class Player(SingleMixin, BasePlayer):
-    fraud = models.BooleanField()
-    revealing = models.BooleanField()
-    vote = models.BooleanField()
+    party = models.StringField()
+    fraud = models.BooleanField(label='Would you like to commit a fraud in this period?')
+    revealing = models.BooleanField(label='Would you like to inform voters that an opposite party is planning a fraud?')
+    vote = models.BooleanField(label='Would you like to vote for your party in this period?')
     party = models.StringField(choices=Constants.parties)
 
     def role(self):
--- fraud/pages.py
+++ fraud/pages.py
@@ -4,10 +4,17 @@ from .models import Constants
 
 
 class FirstWP(WaitPage):
+    # TODO: remember that for non-single-player version this should be only ran once otherwise there will
+    # be no stable group composition
     group_by_arrival_time = 'true'
+
+    after_all_players_arrive = 'after_round_starts'
 
 
 class Fraud(Page):
+    form_model = 'player'
+    form_fields = ['fraud']
+
     def is_displayed(self):
         return self.player.role() == 'candidate' and self.subsession.fraud
 
@@ -17,9 +24,18 @@ class AfterFraudWP(WaitPage):
 
 
 class FraudRevealing(Page):
+    form_model = 'player'
+    form_fields = ['revealing']
 
     def is_displayed(self):
         return self.player.role() == 'candidate' and self.subsession.info
+
+    def vars_for_template(self):
+        if self.player.party == 'Red':
+            other_candidate_decision = self.group.blue_fraud
+        else:
+            other_candidate_decision = self.group.red_fraud
+        return dict(other_candidate_decision=other_candidate_decision)
 
 
 class AfterFraudRevealingWP(WaitPage):
@@ -27,6 +43,9 @@ class AfterFraudRevealingWP(WaitPage):
 
 
 class Voting(Page):
+    form_model = 'player'
+    form_fields = ['vote']
+
     def is_displayed(self):
         return self.player.role() == 'voter'
 
@@ -36,10 +55,16 @@ class ResultsWaitPage(WaitPage):
 
 
 class Results(Page):
-    pass
+    def vars_for_template(self):
+        return dict(
+            num_reds='num_reds',
+            num_blues='num_blues',
+            winner='winner'
+        )
 
 
 page_sequence = [
+    FirstWP,
     Fraud,
     AfterFraudWP,
     FraudRevealing,
